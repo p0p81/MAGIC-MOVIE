56126ac4798f087dcafcfd972442f3835abac71d
p0p81 <geno_popov@yahoo.com>
2024-02-02 11:29:45 +0200

    fixed issue on middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -24,7 +24,7 @@ exports.authMid = async (req, res, next) => {
 
 exports.isAuth = (req, res, next) => {
   if (!req.user) {
-    res.redirect("/login");
+   return res.redirect("/login");
   }
   next();
 };
