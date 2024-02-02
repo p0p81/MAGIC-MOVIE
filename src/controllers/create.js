29f99ef0b4fd01e711cd63356b84ef8e967d55c6
p0p81 <geno_popov@yahoo.com>
2024-02-02 11:39:20 +0200

    add details options for owner

--- src/controllers/create.js
+++ src/controllers/create.js
@@ -27,12 +27,13 @@ router.post("/create", isAuth, async (req, res) => {
 router.get("/movies/:movieId", async (req, res) => {
   const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId).lean();
+  const isOwner = movie.owner == req.user._id;   // using == not ===, cause we are comparing object with string
 
   //movie.rating = new Array(Number(movie.rating)).fill(true);
 
   movie.ratingStars = "&#x2605;".repeat(movie.rating); //TO DO ...this is not good, use hanlebars helpers
 
-  res.render("movie/details", { movie });
+  res.render("movie/details", { movie, isOwner });
 });
 
 
